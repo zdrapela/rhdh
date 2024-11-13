5788d5fadbff3db610be405cbbf62fd715eca2f8
Zbyněk Drápela <zbynek.drapela@gmail.com>
2024-11-13 14:46:46 +0100

    chore(ci): fix AKS e2e tests

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -160,10 +160,10 @@ apply_yaml_files() {
   done
 
   if [[ "$JOB_NAME" == *aks* ]]; then
-    GITHUB_APP_APP_ID=$GITHUB_APP_2_APP_ID
-    GITHUB_APP_CLIENT_ID=$GITHUB_APP_2_CLIENT_ID
-    GITHUB_APP_PRIVATE_KEY=$GITHUB_APP_2_PRIVATE_KEY
-    GITHUB_APP_CLIENT_SECRET=$GITHUB_APP_2_CLIENT_SECRET
+    GITHUB_APP_APP_ID=$GITHUB_APP_3_APP_ID
+    GITHUB_APP_CLIENT_ID=$GITHUB_APP_3_CLIENT_ID
+    GITHUB_APP_PRIVATE_KEY=$GITHUB_APP_3_PRIVATE_KEY
+    GITHUB_APP_CLIENT_SECRET=$GITHUB_APP_3_CLIENT_SECRET
   fi
 
   for key in GITHUB_APP_APP_ID GITHUB_APP_CLIENT_ID GITHUB_APP_PRIVATE_KEY GITHUB_APP_CLIENT_SECRET GITHUB_APP_JANUS_TEST_APP_ID GITHUB_APP_JANUS_TEST_CLIENT_ID GITHUB_APP_JANUS_TEST_CLIENT_SECRET GITHUB_APP_JANUS_TEST_PRIVATE_KEY GITHUB_APP_WEBHOOK_URL GITHUB_APP_WEBHOOK_SECRET KEYCLOAK_CLIENT_SECRET ACR_SECRET GOOGLE_CLIENT_ID GOOGLE_CLIENT_SECRET K8S_CLUSTER_TOKEN_ENCODED OCM_CLUSTER_URL GITLAB_TOKEN; do
@@ -191,11 +191,15 @@ apply_yaml_files() {
   sed -i "s/OCM_CLUSTER_TOKEN: .*/OCM_CLUSTER_TOKEN: ${token}/" "$dir/auth/secrets-rhdh-secrets.yaml"
 
   if [[ "${project}" == *rbac* ]]; then
-    oc apply -f "$dir/resources/config_map/configmap-app-config-rhdh-rbac.yaml" --namespace="${project}"
+    oc create configmap app-config-rhdh --from-file "$dir/resources/config_map/app-config-rhdh-rbac.yaml" --namespace="${project}" --dry-run=client -o yaml | oc apply -f -
+  elif [[ "$JOB_NAME" == *aks* || "$JOB_NAME" == *gke* ]]; then
+    yq 'del(.backend.cache)' "$dir/resources/config_map/app-config-rhdh.yaml" \
+    | kubectl create configmap app-config-rhdh --from-file="app-config-rhdh.yaml"="/dev/stdin" --namespace="${project}" --dry-run=client -o yaml \
+    | kubectl apply -f -
   else
-    oc apply -f "$dir/resources/config_map/configmap-app-config-rhdh.yaml" --namespace="${project}"
+    oc create configmap app-config-rhdh --from-file "$dir/resources/config_map/app-config-rhdh.yaml" --namespace="${project}" --dry-run=client -o yaml | oc apply -f -
   fi
-  oc apply -f "$dir/resources/config_map/configmap-rbac-policy-rhdh.yaml" --namespace="${project}"
+  oc create configmap rbac-policy --from-file "$dir/resources/config_map/rbac-policy.csv" --namespace="${project}" --dry-run=client -o yaml | oc apply -f -
   oc apply -f "$dir/auth/secrets-rhdh-secrets.yaml" --namespace="${project}"
 
   sleep 20 # wait for Pipeline Operator/Tekton pipelines to be ready
@@ -427,7 +431,7 @@ main() {
 
   API_SERVER_URL=$(oc whoami --show-server)
   if [[ "$JOB_NAME" == *aks* ]]; then
-    K8S_CLUSTER_ROUTER_BASE=$(kubectl get svc nginx --namespace app-routing-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    K8S_CLUSTER_ROUTER_BASE=$AKS_INSTANCE_DOMAIN_NAME
   else
     K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
   fi
